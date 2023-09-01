4acb46a95c0e09a9c7755c75a389362faaaa5098
cioccarellia <andrea.cioccarelli01@gmail.com>
2023-09-01 20:47:42 +0200

    Added package queries and dependencies on showcase module to fix #9

--- antipiracy/src/main/kotlin/com/kevlar/antipiracy/detection/vectors/heuristic/HeuristicDataset.kt
+++ antipiracy/src/main/kotlin/com/kevlar/antipiracy/detection/vectors/heuristic/HeuristicDataset.kt
@@ -35,93 +35,93 @@ internal object HeuristicDataset {
         MatchableHeuristicDatasetEntry(
             DatasetEntry.URET_PATCHER,
             DetectionPolicy.packageNames(
-                "uret.jasi2169.patcher",
-                "zone.jasi2169.uretpatcher",
+                """uret.jasi2169.patcher""",
+                """zone.jasi2169.uretpatcher""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.CREEPLAYS_PATCHER,
             DetectionPolicy.packageNames(
-                "org.creeplays.hack"
+                """org.creeplays.hack"""
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.CREE_HACK,
             DetectionPolicy.packageNames(
-                "apps.zhasik007.hack",
+                """apps.zhasik007.hack""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.LEO_PLAYCARDS,
             DetectionPolicy.packageNames(
-                "com.leo.playcard",
+                """com.leo.playcard""",
                 /* com.hakiansatu.leohhf */
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.APP_SARA,
             DetectionPolicy.packageNames(
-                "com.appsara.app",
+                """com.appsara.app""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.XMG,
             DetectionPolicy.packageNames(
-                "com.xmodgame",
+                """com.xmodgame""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.GAME_HACKER,
             DetectionPolicy.packageNames(
-                "org.sbtools.gamehack",
+                """org.sbtools.gamehack""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.GAME_KILLER,
             DetectionPolicy.packageNames(
-                "com.zune.gamekiller",
-                "com.killerapp.gamekiller",
-                "cn.lm.sq",
+                """com.zune.gamekiller""",
+                """com.killerapp.gamekiller""",
+                """cn.lm.sq""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.AGK,
             DetectionPolicy.packageNames(
-                "com.aag.killer",
+                """com.aag.killer""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.CGD,
             DetectionPolicy.packageNames(
-                "com.github.oneminusone.disablecontentguard",
-                "com.oneminusone.disablecontentguard",
+                """com.github.oneminusone.disablecontentguard""",
+                """com.oneminusone.disablecontentguard""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.FREEDOM,
             DetectionPolicy.packageNames(
-                "madkite.freedom",
-                "jase.freedom",
-                "cc.jase.freedom",
-                "cc.madkite.freedom",
-                "cc.cz.madkite.freedom",
+                """madkite.freedom""",
+                """jase.freedom""",
+                """cc.jase.freedom""",
+                """cc.madkite.freedom""",
+                """cc.cz.madkite.freedom""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.ROOT_CLOAK,
             DetectionPolicy.packageNames(
-                "com.devadvance.rootcloak",
-                "com.devadvance.rootcloakplus"
+                """com.devadvance.rootcloak""",
+                """com.devadvance.rootcloakplus"""
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.HIDE_ROOT,
             DetectionPolicy.packageNames(
-                "com.formyhm.hideroot",
-                "com.formyhm.hiderootpremium",
-                "com.amphoras.hidemyroot",
-                "com.amphoras.hidemyrootadfree",
-                "com.zachspong.temprootremovejb",
+                """com.formyhm.hideroot""",
+                """com.formyhm.hiderootpremium""",
+                """com.amphoras.hidemyroot""",
+                """com.amphoras.hidemyrootadfree""",
+                """com.zachspong.temprootremovejb""",
             )
         ),
     )
@@ -134,86 +134,86 @@ internal object HeuristicDataset {
         MatchableHeuristicDatasetEntry(
             DatasetEntry.APTOIDE,
             DetectionPolicy.packageNames(
-                "cm.aptoide.pt",
+                """cm.aptoide.pt""",
             )
         ),
 
         MatchableHeuristicDatasetEntry(
             DatasetEntry.HAPPYMOD,
             DetectionPolicy.packageNames(
-                "com.happymod.apk",
-                "happygames.io",
+                """com.happymod.apk""",
+                """happygames.io""",
             )
         ),
 
         MatchableHeuristicDatasetEntry(
             DatasetEntry.BLACK_MART,
             DetectionPolicy.packageNames(
-                "org.blackmart.market",
-                "com.blackmartalpha",
+                """org.blackmart.market""",
+                """com.blackmartalpha""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.MOB_GENIE,
             DetectionPolicy.packageNames(
-                "com.mobogenie",
+                """com.mobogenie""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.ONE_MOBILE,
             DetectionPolicy.packageNames(
-                "me.onemobile.android",
+                """me.onemobile.android""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.GET_APK,
             DetectionPolicy.packageNames(
-                "com.repodroid.app",
+                """com.repodroid.app""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.GET_JAR,
             DetectionPolicy.packageNames(
-                "com.getjar.reward",
+                """com.getjar.reward""",
             )
         ),
 
         MatchableHeuristicDatasetEntry(
             DatasetEntry.SLIDE_ME,
             DetectionPolicy.packageNames(
-                "com.slideme.sam.manager",
+                """com.slideme.sam.manager""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.AC_MARKET,
             DetectionPolicy.packageNames(
-                "ac.market.store",
+                """ac.market.store""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.APP_CAKE,
             DetectionPolicy.packageNames(
-                "net.appcake",
-                "com.appcake",
+                """net.appcake""",
+                """com.appcake""",
             )
         ),
 
         MatchableHeuristicDatasetEntry(
             DatasetEntry.Z_MARKET,
             DetectionPolicy.packageNames(
-                "com.zmapp",
+                """com.zmapp""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.MOBILISM,
             DetectionPolicy.packageNames(
-                "org.mobilism.android",
+                """org.mobilism.android""",
             )
         ),
         MatchableHeuristicDatasetEntry(
             DatasetEntry.AIOD,
             DetectionPolicy.packageNames(
-                "com.allinone.free",
+                """com.allinone.free""",
             )
         ),
     )
